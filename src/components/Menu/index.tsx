f60b113e1ca5ff559673b26cf37a62549d75b29d
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-11-21 06:51:15 -0300

    feat: create Menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,3 +1,40 @@
+import { FlatList } from "react-native";
+import { products } from "../../mocks/products";
+import { formatCurrency } from "../../utils/formatCurrency";
+import { Text } from "../Text";
+import {
+  ProductImage,
+  ProductContainer,
+  ProductDetails,
+  Separator,
+} from "./styles";
+
 export function Menu() {
-  return null;
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={(product) => product._id}
+      style={{ marginTop: 32 }}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
+      ItemSeparatorComponent={Separator}
+      renderItem={({ item: product }) => (
+        <ProductContainer>
+          <ProductImage
+            source={{
+              uri: `http://192.168.0.105:3333/uploads/${product.imagePath}`,
+            }}
+          />
+          <ProductDetails>
+            <Text weight="600">{product.name}</Text>
+            <Text size={14} color="#666" style={{ marginVertical: 8 }}>
+              {product.description}
+            </Text>
+            <Text weight="600" size={14}>
+              {formatCurrency(product.price).replace("R$", "R$ ")}
+            </Text>
+          </ProductDetails>
+        </ProductContainer>
+      )}
+    />
+  );
 }
