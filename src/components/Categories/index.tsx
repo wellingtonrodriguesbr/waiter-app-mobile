58a4e1edb2ca930124c3414278b0417f18d7cd8d
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-11-21 06:50:59 -0300

    feat: create Categories component

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,20 +1,38 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { categories } from "../../mocks/categories";
 import { Text } from "../Text";
 import { Category, Icon } from "./styles";
 
 export function Categories() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  function handleSelectCategory(categoryId: string) {
+    const category = selectedCategory === categoryId ? "" : categoryId;
+    setSelectedCategory(category);
+  }
+
   return (
-    <>
-      {categories.map((category) => (
-        <Category key={category._id}>
-          <Icon>
-            <Text>{category.icon}</Text>
-          </Icon>
-          <Text size={14} weight="600">
-            {category.name}
-          </Text>
-        </Category>
-      ))}
-    </>
+    <FlatList
+      data={categories}
+      horizontal
+      keyExtractor={(category) => category._id}
+      contentContainerStyle={{ paddingRight: 24 }}
+      renderItem={({ item: category }) => {
+        const isSelected = selectedCategory === category._id;
+
+        return (
+          <Category onPress={() => handleSelectCategory(category._id)}>
+            <Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>{category.icon}</Text>
+            </Icon>
+            <Text size={14} weight="600" opacity={isSelected ? 1 : 0.5}>
+              {category.name}
+            </Text>
+          </Category>
+        );
+      }}
+      showsHorizontalScrollIndicator={false}
+    />
   );
 }
--- src/components/Categories/styles.ts
+++ src/components/Categories/styles.ts
@@ -1,4 +1,21 @@
 import styled from "styled-components/native";
+import { Platform } from "react-native";
 
-export const Category = styled.View``;
-export const Icon = styled.View``;
+const isAndroid = Platform.OS === "android";
+
+export const Category = styled.TouchableOpacity`
+  align-items: center;
+  margin-left: 24px;
+`;
+
+export const Icon = styled.View`
+  background-color: #fff;
+  width: 44px;
+  height: 44px;
+  border-radius: 22px;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 8px;
+  box-shadow: 0px 2px 1px rgba(0, 0, 0, ${isAndroid ? 0.4 : 0.1});
+  elevation: 2;
+`;
