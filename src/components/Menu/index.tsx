a5f8777c1ab20dbfd1ea20211364e521f70d713b
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-11-22 06:24:59 -0300

    feat: finished Menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,12 +1,14 @@
 import { FlatList } from "react-native";
 import { products } from "../../mocks/products";
 import { formatCurrency } from "../../utils/formatCurrency";
+import { PlusCircle } from "../Icons/PlusCircle";
 import { Text } from "../Text";
 import {
   ProductImage,
   ProductContainer,
   ProductDetails,
   Separator,
+  AddToCartButton,
 } from "./styles";
 
 export function Menu() {
@@ -33,6 +35,10 @@ export function Menu() {
               {formatCurrency(product.price).replace("R$", "R$ ")}
             </Text>
           </ProductDetails>
+
+          <AddToCartButton>
+            <PlusCircle />
+          </AddToCartButton>
         </ProductContainer>
       )}
     />
